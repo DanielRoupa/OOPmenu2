a12dc36008377d6b4edc87b99fb589f0c176904e
Dan <Dan@DESKTOP-JB3AORE>
2023-11-17 22:59:53 +0100

    Add project files.

---
+++ OOPmenu/Class1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPmenu
+{
+    public class Menu
+    {
+        public string Nadpis = "";
+        public ConsoleColor barvaokraje = ConsoleColor.Green;
+        public ConsoleColor barvapozadi = ConsoleColor.Black;
+        public ConsoleColor barvatextu = ConsoleColor.Yellow;
+        public ConsoleColor barvaokurzoru = ConsoleColor.Blue;
+        public int sirka = 25;
+        public string[] polozky = new string[5];
+
+        public void zobraz()
+        {
+            zobrazokraj();
+            zobraznadpis();
+            //zobrazkurzor();
+            zobrazpolozky();
+        }
+        private void zobraznadpis()
+        {
+            Console.BackgroundColor = barvapozadi;
+            Console.ForegroundColor = barvatextu;
+            Console.SetCursorPosition(3, 1);
+            Console.Write(Nadpis);
+        }
+        private void zobrazpolozky()
+        {
+            Console.BackgroundColor = barvapozadi;
+            Console.ForegroundColor = barvatextu;
+            //Console.SetCursorPosition(3, 3);
+            //Console.Write(polozky[0]);
+            //Console.SetCursorPosition(3, 4);
+            //Console.Write(polozky[1]);
+            //Console.SetCursorPosition(3, 5);
+            //Console.Write(polozky[2]);
+            //Console.SetCursorPosition(3, 6);
+            //Console.Write(polozky[3]);
+            for (int i = 0; i < 4; i++)
+            {
+                Console.SetCursorPosition(3, i + 3);
+                Console.Write(polozky[i]);
+            }
+        }
+
+        private void zobrazokraj()
+        {
+            Console.BackgroundColor = barvapozadi;
+            Console.ForegroundColor = barvaokraje;
+            Console.Write("╔");
+            
+            Console.Write(new String('═',sirka));
+
+            Console.WriteLine("╗");
+            Console.Write("║");
+            Console.Write(new String(' ', sirka));
+            Console.WriteLine("║");
+            Console.Write("╠");
+            Console.Write(new String('═', sirka));
+            Console.WriteLine("╣");
+            for (int i = 0; i < polozky.Length; i++)
+            {
+                Console.Write("║");
+                Console.Write(new String(' ', sirka));
+                Console.WriteLine("║");
+            }
+            Console.Write("╚");
+            
+            Console.Write(new String('═', sirka));
+            Console.WriteLine("╝");
+
+
+
+
+
+        }
+    }
+}
---
+++ OOPmenu/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Console.CursorVisible = false;
+//Console.ForegroundColor = ConsoleColor.Green;
+//Console.WriteLine("╔════════════════════╗");
+//Console.WriteLine("║                    ║");
+//Console.WriteLine("╠════════════════════╣");
+//Console.WriteLine("║                    ║");
+//Console.WriteLine("║                    ║");
+//Console.WriteLine("║                    ║");
+//Console.WriteLine("╚════════════════════╝");
+//Console.ForegroundColor = ConsoleColor.White;
+//Console.SetCursorPosition(6,1);
+//Console.Write("SUPER MENU");
+//Console.SetCursorPosition(6, 3);
+//Console.Write("Položka 1");
+//Console.BackgroundColor = ConsoleColor.Yellow;
+//Console.ForegroundColor = ConsoleColor.Black;
+//Console.SetCursorPosition(1, 4);
+//Console.Write("                    ");
+//Console.SetCursorPosition(6, 4);
+//Console.Write("Položka 2");
+//Console.BackgroundColor = ConsoleColor.Black;
+//Console.ForegroundColor = ConsoleColor.White;
+
+//Console.SetCursorPosition(6, 5);
+//Console.Write("Položka 3");
+//Console.ReadKey();
+using OOPmenu;
+Console.CursorVisible = false;
+Menu menu = new Menu();
+//menu.zobraz();
+menu.polozky =new string[5];
+
+menu.Nadpis = "Moje Menu";
+menu.polozky = new string[] { "New York", "London", "Mumbai", "Chicago" }; 
+menu.zobraz();
+Console.ReadKey();
